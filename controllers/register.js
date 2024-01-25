d90b2001afa38b68caf54d0d8b8206a355399e8b
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-25 17:02:45 +0100

    update

--- controllers/register.js
+++ controllers/register.js
@@ -31,7 +31,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register', err))
+    .catch((err) => {res.status(400).json('unable to register', err)})
 }
 
 module.exports = {
