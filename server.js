29c5761f6673b7ac4c8659dda2ba3999db5406b8
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-26 11:32:12 +0100

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DB_URL,
   }
 });
 
