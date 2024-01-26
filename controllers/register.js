fc6f6fd6839753fb4af0d920e3ed1a982ead3071
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-26 11:51:49 +0100

    update

--- controllers/register.js
+++ controllers/register.js
@@ -15,6 +15,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .into('login')
       .returning('email')
       .then(loginEmail => {
+        console.log(loginEmail)
         return trx('users')
           .returning('*')
           .insert({
