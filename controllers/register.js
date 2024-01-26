bba37a966194f004a1416c3b0211062fb1efd228
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-26 12:02:48 +0100

    update

--- controllers/register.js
+++ controllers/register.js
@@ -6,37 +6,37 @@ const handleRegister = (req, res, db, bcrypt) => {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      console.log("trx started")
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then((loginEmail) => {
-        console.log(loginEmail)
-        return trx('users')
-          .returning('*')
-          .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            console.log("returning user")
-            res.json(user[0]);
-          })
+  db.transaction(trx => {
+    console.log("trx started")
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then((loginEmail) => {
+      console.log(loginEmail)
+      return trx('users')
+        .returning('*')
+        .insert({
+          // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+          // loginEmail[0] --> this used to return the email
+          // TO
+          // loginEmail[0].email --> this now returns the email
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          console.log("returning user")
+          res.json(user[0]);
+        })
       })
       .then(trx.commit)
       .catch(trx.rollback)
     })
     .catch((err) => {
-      console.log("error registering")
+      console.log("error registering:", err)
       res.status(400).json('unable to register')})
 }
 
